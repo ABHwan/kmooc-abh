2b8f16234d7a91b2685eb2e3964f7717c5d939b9
abh <dksqhrghks12@peoplelife.co.kr>
2022-11-23 21:09:46 +0900

    kmooc 13주차 과제

---
+++ src/js/api.js
@@ -0,0 +1,116 @@
+let menu = {}
+let menuTitle = []
+const navList = [
+	{
+		text: 'Open Api',
+		event: () => openApi()
+	},
+	{
+		text: 'Node',
+		event: () => node()
+	}
+]
+
+init()
+
+function init() {
+	createNav()
+	openApi()
+}
+
+function createNav() {
+	const ul = document.querySelector('.nav-ul')
+	navList.forEach(nav => {
+		const li = document.createElement('li')
+		li.innerText = nav.text
+		li.addEventListener('click', () => nav.event())
+
+		ul.appendChild(li)
+	})
+
+}
+
+async function openApi() {
+	await $.ajax({
+		url:'http://smart.handong.edu/api/service/menu',
+		type:'GET',
+		dataType: 'json',
+		success: function(data){
+			menu = data
+			menuTitle = Object.keys(data)
+			getMenu(menuTitle[0])
+		}// end
+	});// end ajax
+}
+
+function getMenu(key) {
+	const item = menu[key]
+	const contents = document.querySelector('.contents')
+
+	const card = document.createElement('div')
+	const cardTitle = document.createElement('div')
+	const cardContent = document.createElement('div')
+
+	card.classList.add('card')
+
+	cardTitle.classList.add('card-title')
+	cardTitle.innerText = key
+
+	createTitleButton(cardTitle)
+
+	cardContent.classList.add('card-content')
+
+	const table = document.createElement('table')
+	const thead = document.createElement('thead')
+	const tbody = document.createElement('tbody')
+
+	createTh(thead, '메뉴')
+	createTh(thead, '칼로리')
+	createTh(thead, '가격')
+
+	menu[key].forEach(item => {
+		const tr = document.createElement('tr')
+		createTd(tr, item.menu_kor ? item.menu_kor : '-')
+		createTd(tr, item.kcal ? item.kcal : '-')
+		createTd(tr, item.price ? `${item.price}원` : '0원')
+		tbody.appendChild(tr)
+	})
+	table.appendChild(thead)
+	table.appendChild(tbody)
+
+	cardContent.appendChild(table)
+
+	card.appendChild(cardTitle)
+	card.appendChild(cardContent)
+
+	contents.innerHTML = ''
+	contents.appendChild(card)
+}
+
+function createTh(thead, text) {
+	const th = document.createElement('th')
+	th.innerText = text
+	thead.appendChild(th)
+}
+
+function createTd(tr, text) {
+	const td = document.createElement('td')
+	td.innerText = text
+	tr.appendChild(td)
+}
+
+function createTitleButton(cardTitle) {
+	menuTitle.forEach(title => {
+		const button = document.createElement('button')
+		button.innerText = title
+		button.addEventListener('click', () => {
+			getMenu(title)
+		})
+
+		cardTitle.appendChild(button)
+	})
+}
+
+function node() {
+	alert('준비 중')
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  // The entry point file described above
+  entry: {
+    main: './src/js/index.js',
+    api: './src/js/api.js',
+  },
+  // The location of the build folder described above
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    // filename: 'bundle.js'
+    filename: '[name].bundle.js',
+    clean: {
+      keep: filename => {
+        console.log("debug", filename)
+        
+        return ['index.html', 'main.css'].includes(filename)
+      }
+    }
+  },
+  // Optional and for development only. This provides the ability to
+  // map the built code back to the original source format when debugging.
+  devtool: 'eval-source-map',
+};
